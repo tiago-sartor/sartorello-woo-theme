c411f53da446c523b7990b723cc837890fad18d8
Tiago Sartor <contact.me@tiagosartor.dev>
2026-09-11 13:14:56 -0300

    Update loading spinner border thickness for improved visibility

--- woocommerce/components/loading-spinner.php
+++ woocommerce/components/loading-spinner.php
@@ -7,5 +7,5 @@ $size = $args['size'] ?? false;
 ?>
 
 <div x-cloak x-show="loading" class="loading-spinner absolute inset-0 z-10 flex items-center justify-center bg-white/50" aria-hidden="true">
-    <span class="<?php echo esc_attr($size ?: 'size-18'); ?> border-3 border-neutral-300 border-r-black rounded-full animate-spin"></span>
+    <span class="<?php echo esc_attr($size ?: 'size-18'); ?> animate-spin rounded-full border-4 border-neutral-300 border-r-black"></span>
 </div>
